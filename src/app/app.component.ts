9155783ad0693cafe1975abac7031c235a0bad62
khushburaghu <khushi.raghu.kr@gmail.com>
2021-09-28 20:29:37 +0530

    create flip cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('flipState', [
+      state('active', style({
+        transform: 'rotateY(179deg)'
+      })),
+      state('inactive', style({
+        transform: 'rotateY(0)'
+      })),
+      transition('active => inactive', animate('500ms ease-out')),
+      transition('inactive => active', animate('500ms ease-in'))
+    ])
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-test-task';
+  flip = 'inactive';
+
+  constructor() { }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
+  // tslint:disable-next-line:typedef
+  public toggleFlip() {
+    this.flip = (this.flip === 'inactive') ? 'active' : 'inactive';
+  }
 }
